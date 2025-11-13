dd63e29b1cb7b4a3f167acf1d60a10e6f9b44d50
Natalia Perinova <natashaperinova@gmail.com>
2025-11-13 17:21:38 +0400

    first commit

---
+++ animation.js
@@ -0,0 +1,27 @@
+const btn = document.getElementById('startBtn');
+const form = document.querySelector('.form-container');
+const star = document.getElementById('star');
+
+btn.addEventListener('click', () => {
+  
+  form.style.display = 'none';
+
+ 
+  star.classList.add('animate');
+});
+const button = document.querySelector('.back');
+
+
+star.addEventListener('animationend', () => {
+  button.classList.add('visible');
+});
+
+button.addEventListener('click', () => {
+  window.location.href = 'wish.html'; 
+});
+
+const message = document.getElementById('message');
+
+star.addEventListener('animationend', () => {
+  message.style.opacity = 1;
+});
