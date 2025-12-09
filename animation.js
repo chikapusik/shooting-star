243a4c89b2c86f8db075401590bdfe283c26654b
Natalia Perinova <natashaperinova@gmail.com>
2025-12-09 17:09:32 +0400

    fixed the message

--- animation.js
+++ animation.js
@@ -11,21 +11,21 @@ let started = false;
 function startHandler(e) {
   if (started) return;
   started = true;
-  // предотвращаем нежелательное поведение (например, фокус/зум)
+ 
   if (e && e.type === 'touchstart') e.preventDefault();
 
   form.style.display = 'none';
-  // делаем старт анимации
+ 
   star.classList.add('animate');
 }
 
-// два варианта события: touchstart + click (чтобы покрыть все браузеры)
+
 btn.addEventListener('click', startHandler, { passive: true });
 btn.addEventListener('touchstart', startHandler, { passive: false });
 
 star.addEventListener('animationend', () => {
   back.classList.add('visible');
-  // показываем сообщение (opacity:1)
+  
   message.style.opacity = 1;
 });
 
